797ce4ad8e2e7d100b27d505b20bb510b415d3b9
Kostiantyn Romanovskyi <romanovskiyk97@gmail.com>
2022-12-07 18:29:29 +0200

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,78 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
+
+const galaryEl = document.querySelector(".gallery");
+
+const cardsMarkup = onCreateGalaryElements(galleryItems);
+
+galaryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+
+// Функція створення айтемів галереї
+function onCreateGalaryElements(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+               <a class="gallery__link" href="${original}">
+                  <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                     />
+                       </a>
+                </div>
+        `;
+    })
+    .join("");
+}
+
+galaryEl.addEventListener("click", onGetOriginalPicture);
+
+// Функція відкриття великої картинки
+function onGetOriginalPicture(event) {
+  onCheckImgElement(event);
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  removeTransitionOnLink(event);
+
+  galaryEl.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+  //   function onCloseEsc(event) {
+  //     if (event.code === "Escape") {
+  //       instance.close();
+  //     }
+  //     console.log(instance.close());
+  //   }
+  //   onCloseEsc(event);
+}
+
+// function onCloseEsc(event) {
+//   if (event.code === "Escape") {
+//     instance.close();
+//   }
+
+//   console.log(instance.close());
+// }
+
+// Функція перевірки таргету
+function onCheckImgElement(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+}
+
+// функція відключення переходу по посиланню
+function removeTransitionOnLink(event) {
+  event.preventDefault();
+}
 
 console.log(galleryItems);
